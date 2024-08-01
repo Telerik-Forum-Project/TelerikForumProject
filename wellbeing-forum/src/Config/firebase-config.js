0293d0e6b820490a94844151fb67d01d5d95c1d7
Leharov <viktorleharov@gmail.com>
2024-08-01 15:37:14 +0300

    firebase update config

--- wellbeing-forum/src/Config/firebase-config.js
+++ wellbeing-forum/src/Config/firebase-config.js
@@ -1,20 +1,25 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDataBase } from "firebase/database";
-
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+ 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMLZtke_5jTEWWN53XuYRuyekCUDWtWeA",
-  authDomain: "telerikforumproject.firebaseapp.com",
-  projectId: "telerikforumproject",
-  storageBucket: "telerikforumproject.appspot.com",
-  messagingSenderId: "654185871956",
-  appId: "1:654185871956:web:d571aa3c8c4749bb524bc1",
-  databaseURL: "https://telerikforumproject-default-rtdb.europe-west1.firebasedatabase.app/"
+  apiKey: "AIzaSyDILU8GuhT-fKVwnsjpl58CE4TsdgNS8Sg",
+  authDomain: "wellbeing-forum-telerk.firebaseapp.com",
+  projectId: "wellbeing-forum-telerk",
+  storageBucket: "wellbeing-forum-telerk.appspot.com",
+  messagingSenderId: "402944250987",
+  appId: "1:402944250987:web:cf707ed780bdadf3950d2e",
+  databaseURL: 'https://wellbeing-forum-telerk-default-rtdb.europe-west1.firebasedatabase.app/'
 };
-
+ 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+ 
+// the Firebase authentication handler
 export const auth = getAuth(app);
-
-export const db = getDataBase(app);
+// the Realtime Database handler
+export const db = getDatabase(app);
