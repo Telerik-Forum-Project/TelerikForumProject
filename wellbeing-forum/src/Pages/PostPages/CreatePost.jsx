459be0f10c583dc07547671c065ce86c350f0234
V Manin <victormanin151@gmail.com>
2024-08-01 13:06:57 +0300

    added func to createpost.jsx

--- wellbeing-forum/src/Pages/PostPages/CreatePost.jsx
+++ wellbeing-forum/src/Pages/PostPages/CreatePost.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 
 export default function CreatePost() {
 
+    const [newTopicName, setNewTopicName] = useState("");
+    const [newTopicDescription, setNewTopicDescription] = useState("");
+
+    function handleInputTopicChange(e) {
+        setNewTopicName(e.target.value);
+    }
+
+    function handleInputDescriptionChange(e) {
+        setNewTopicDescription(e.target.value);
+    }
 
 
     return (<>
@@ -9,13 +20,15 @@ export default function CreatePost() {
             <label className="title-label">Title: 
                 <input type="text" 
                        placeholder="Enter title here..."
-                      />
+                       value={newTopicName}
+                       onChange={handleInputTopicChange}/>
             </label><br />
             <br />
             <label className="comment-label">Description: 
                 <textarea type="text" 
                           placeholder="Enter description here..."
-                          />
+                          value={newTopicDescription}
+                          onChange={handleInputDescriptionChange}/>
             </label><br />
             <button>Create new Post</button>
         </form>
