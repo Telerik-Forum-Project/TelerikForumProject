35241dc3573a638ecf9c7bf0cb24e3aadb53b974
Leharov <viktorleharov@gmail.com>
2024-08-01 13:58:25 +0300

    implemented Post.jsx and created LikeButton component

---
+++ wellbeing-forum/src/Components/Post.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+export default function Post({ title, content, author, date, likes }) {
+
+
+    return (
+      <div className="post">
+        <h2>{title}</h2>
+        <p>{content}</p>
+        <small>Posted by {author} on {date}</small>
+        <div>
+          <span>{likes} Likes</span>
+          {/* LikeButton */}
+        </div>
+      </div>
+    );
+  }
+
+  Post.propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+  };
