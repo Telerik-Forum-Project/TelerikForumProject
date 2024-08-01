f0b571453cd3bf89ab1685feaecca21b5c48f87d
Leharov <viktorleharov@gmail.com>
2024-08-01 14:23:45 +0300

    quickfix for the Header import in app.jsx

--- wellbeing-forum/src/App.jsx
+++ wellbeing-forum/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Home from './Pages/MainPages/Home';
-import Header from './Components/Header';
+import Header from './Components/MainComponents/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from '../src/Pages/MainPages/About';
 import Fitness from '../src/Pages/MainPages/Fitness';
 import Food from '../src/Pages/MainPages/Food';
 import Lifestyle from '../src/Pages/MainPages/Lifestyle';
-import Footer from './Components/Footer';
+import Footer from './Components/MainComponents/Footer';
 import CreatePost from '../src/Pages/PostPages/CreatePost';
 
 function App() {
