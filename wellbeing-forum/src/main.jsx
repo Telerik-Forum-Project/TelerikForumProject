52f636df47c31d80757fa9af58dda2d5439432f2
Leharov <viktorleharov@gmail.com>
2024-08-07 13:36:53 +0300

    wrapped the app in  authManager

--- wellbeing-forum/src/main.jsx
+++ wellbeing-forum/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthManager } from './state/auth.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthManager>
+      <App />
+    </AuthManager>
   </React.StrictMode>,
 )
