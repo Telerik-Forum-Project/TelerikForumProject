fa8a5a3b44877fc7943438408d05b413a22befed
Leharov <viktorleharov@gmail.com>
2024-08-13 10:40:45 +0300

    most commented css

--- wellbeing-forum/src/pages/postPages/MostCommented10Posts.jsx
+++ wellbeing-forum/src/pages/postPages/MostCommented10Posts.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllPosts, sortPostsByCommentCount } from "../../services/posts.service";
+import './MostCommented10Posts.css'
 
 export default function MostCommented10Posts() {
 
@@ -25,7 +26,7 @@ export default function MostCommented10Posts() {
                 <h1>Most Commented</h1>
                 <div>
                     {posts.map(post => (
-                        <div key={post.id}>
+                        <div key={post.id} className="post-item">
                             <h2> <Link to={`/singlepost/${post.id}`}>{post.title}</Link></h2>
                             <p>Comments: {post.commentCount}</p>
                         </div>
