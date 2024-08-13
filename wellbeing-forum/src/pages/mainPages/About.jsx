33daa60aaa82c1e490180ba441389151eb35152d
Leharov <viktorleharov@gmail.com>
2024-08-13 11:19:37 +0300

    creators pics

--- wellbeing-forum/src/pages/mainPages/About.jsx
+++ wellbeing-forum/src/pages/mainPages/About.jsx
@@ -1,4 +1,7 @@
 import './About.css';
+import maninImage from '../../img/maninImage.webp';
+import viktorImage from '../../img/viktorImage.jpg';
+import trayanImage from '../../img/trayanImage.jpg';
 
 export default function About() {
 
@@ -10,12 +13,15 @@ export default function About() {
         <h1>Meet the creators:</h1>
         <div id="creator-container">
             <div>
+            <img src={maninImage} alt="manin" className="creator-image" />
             <h3>Victor Manin</h3>
             </div>
             <div>
+            <img src={viktorImage} alt="leharov" className="creator-image" />
             <h3>Viktor Leharov</h3>
             </div>
             <div>
+            <img src={trayanImage} alt="trufev" className="creator-image" />
             <h3>Trayan Trufev</h3>
             </div>
         </div>
