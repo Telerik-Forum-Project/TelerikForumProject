4207f948805eb0b4e1f5071c94ab14342c059e0a
Leharov <viktorleharov@gmail.com>
2024-08-13 10:47:41 +0300

    top10recent temporary

--- wellbeing-forum/src/pages/postPages/Top10RecentPosts.jsx
+++ wellbeing-forum/src/pages/postPages/Top10RecentPosts.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getRecentPostsRealtime } from "../../services/posts.service";
 import { Link } from "react-router-dom";
+import './Top10RecentPosts.css'
 
 export default function Top10RecentPosts() {
   const [recentPosts, setRecentPosts] = useState([]);
@@ -15,7 +16,7 @@ export default function Top10RecentPosts() {
         <h1>Latest Posts</h1>
         <div>
           {recentPosts.map(post => (
-            <div key={post.id}>
+            <div key={post.id} className="post-item">
               <h2> <Link to={`/singlepost/${post.id}`}>{post.title}</Link></h2>
               <p>{new Date(post.createdOn).toLocaleString()}</p>
               <p>{post.content}</p>
