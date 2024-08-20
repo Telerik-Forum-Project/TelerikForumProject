7251f11ee9b84e5cea72871623d47beab609bd78
V Manin <victormanin151@gmail.com>
2024-08-20 11:15:23 +0300

    small fix

--- wellbeing-forum/src/components/mainComponents/Header.jsx
+++ wellbeing-forum/src/components/mainComponents/Header.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import {  useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { AppContext } from "../../state/app.context";
 import { logoutUser } from "../../services/authenticate-service"
 import './Header.css';
